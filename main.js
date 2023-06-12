f51b8b2a822223055dd676d54e6baf8945b2f42e
max-mahmud <newmahmud24@gmail.com>
2023-06-12 20:40:07 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const seconds = document.getElementById("seconds");
+const ampm = document.getElementById("ampm");
+
+function formatTime(time) {
+    return time.toString().padStart(2, "0");
+}
+
+
+function isAmPm(hour) {
+    return hour > 12 ? "PM" : "AM";
+}
+
+function clock() {
+    const date = new Date();
+
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    hour.textContent = formatTime(h);
+    minute.textContent = formatTime(m);
+    seconds.textContent = formatTime(s);
+    ampm.textContent = isAmPm(h);
+}
+
+setInterval(clock, 1000);
