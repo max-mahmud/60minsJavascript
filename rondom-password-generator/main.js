ede9e129b9cdb4a148f77f595b19f8c97867272d
max-mahmud <newmahmud24@gmail.com>
2023-06-16 18:12:08 +0600

    password-generator

--- rondom-password-generator/main.js
+++ rondom-password-generator/main.js
@@ -1,37 +1,54 @@
-const words = ['seat', 'pen', 'broad', 'vapor', 'ocean',
-    'red', 'plate', 'late', 'that', 'ring', 'swim', 'shown',
-    'path', 'law', 'list', 'hard', 'plate', 'block', 'two',
-    'pupil', 'were', 'lot', 'pay', 'would', 'tired', 'dull',
-    'mud', 'sky', 'grew', 'hard', 'ill', 'frame',
-    'sport', 'did', 'many', 'been', 'page', 'year', 'trail',
-    'earth', 'are', 'while', 'off', 'town', 'doing', 'size',
-    'steel', 'sale', 'swam', 'put', 'zero', 'week', 'mill',
-    'past', 'aside', 'her', 'cent', 'box', 'fuel', 'block',
-    'those', 'late', 'sun', 'map', 'silk', 'lady', 'meant',
-    'still', 'shine', 'range', 'loud', 'fox', 'gate', 'slide',
-    'each', 'nails', 'flag', 'exist', 'door', 'luck', 'down',
-    'poem', 'depth', 'press', 'crowd', 'herd', 'drink', 'worry',
-    'dried', 'dig', 'new', 'rest', 'play', 'win', 'strong'];
+const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerSet = "abcdefghijklmnopqrstuvwxyz"
+const numberSet = "1234567890"
+const symbolSet = "~!@#$%^&*()_+/"
 
-function getPassword() {
-    const chars = '0123456789!@#$%&';
-    const clength = chars.length;
-    let password = '';
+// selectors
+const passBox = document.getElementById("pass-box")
+const totalChar = document.getElementById("total-char")
+const upperInput = document.getElementById("upper-case")
+const lowerInput = document.getElementById("lower-case")
+const numberInput = document.getElementById("numbers")
+const symbolInput = document.getElementById("symbols")
 
-    while (password.length < 20) {
-        password = password.concat(randomword(chars.length));
-        password = password.concat(chars[randomNumber(clength)]);
+
+const getRandomData = (dataSet) => {
+    return dataSet[Math.floor(Math.random() * dataSet.length)]
+}
+
+
+
+const generatePass =(password = "")=>{
+    if(upperInput.checked){
+        password += getRandomData(upperSet)
     }
-    password = password.substring(0, 16);
-
-    document.getElementById('password').value = password;
+    if (lowerInput.checked) {
+        password += getRandomData(lowerSet)
+    }
+    if (numberInput.checked) {
+        password += getRandomData(numberSet)
+    }
+    if (symbolInput.checked) {
+        password += getRandomData(symbolSet)
+    }
+    if(password.length < totalChar.value) {
+        return generatePass(password)
+    }
+    passBox.innerHTML =truncateString(password, totalChar.value);
 }
+generatePass()
 
-function randomNumber (len) {
-    return Math.floor(Math.random() * len);
-}
+document.getElementById('btn').addEventListener("click",
+    function(){
+        generatePass()
+    }
+)
 
-function randomword() {
-    const number = randomNumber(words.length)
-    return words[number];
+function truncateString(str, num) {
+    if (str.length > num) {
+        let subStr = str.substring(0, num);
+        return subStr;
+    } else {
+        return str;
+    }
 }
